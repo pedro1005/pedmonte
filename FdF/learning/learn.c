73db6c56dd5282bfe5323778c89dfc470d195c97
Pedro Alexandre Monteiro Rodrigues Da Costa <pedmonte@c1r12s7.42porto.com>
2023-07-27 16:26:24 +0100

    27 07 2023

--- FdF/learning/learn.c
+++ FdF/learning/learn.c
@@ -21,30 +21,46 @@ int	ft_sqrt(double nb)
 
 void draw_line(void *mlx, void *win, int beginX, int endX, int beginY, int endY, int color)
 {
-    /*
-    double deltaX = endX - beginX;
-    double deltaY = endY - beginY;
+    // Bresenman's algorythm
+    int deltaX = abs(endX - beginX);
+    int deltaY = abs(endY - beginY);
+    int x_i, y_i;
 
-    //int pixels = ft_sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    if (beginX < endX)
+        x_i = 1;
+    else
+        x_i = -1;
 
-    int pixels = abs((int)deltaX) > abs((int)deltaY) ? abs((int)deltaX) : abs((int)deltaY);
-    deltaX /= pixels;
-    deltaY /= pixels;
+    if (beginY < endY)
+        y_i = 1;
+    else
+        y_i = -1;
 
-    double pixelX = beginX;
-    double pixelY = beginY;
-    while (pixels)
+    int error = deltaX - deltaY;
+    int x = beginX;
+    int y = beginY;
+
+    while (1)
     {
-        mlx_pixel_put(mlx, win, (int)pixelX, (int)pixelY, color);
-        pixelX += deltaX;
-        pixelY += deltaY;
-        --pixels;
+        mlx_pixel_put(mlx, win, x, y, color);
+        
+        if (x == endX && y == endY)
+            break;
+
+        int error2 = 2 * error;
+        
+        if (error2 > -deltaY)
+        {
+            error -= deltaY;
+            x += x_i;
+        }
+
+        if (error2 < deltaX)
+        {
+            error += deltaX;
+            y += y_i;
+        }
     }
-    */
-
-   int  deltaX = endX - beginX;
-   int  deltaY = endY - beginY;
-
 }
 
 int main()
@@ -52,7 +68,6 @@ int main()
     void    *mlx = mlx_init();
     void    *win = mlx_new_window(mlx, 640, 640, "Learning...Draw line");
     
-    //mlx_pixel_put(mlx, win, 640/2, 640/2, 0xFF0000);
     draw_line(mlx, win, 0, 640, 0, 640, 0xFF0000);
     mlx_loop(mlx);
 }

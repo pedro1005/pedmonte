c4c060b94bb8eac5e359da471589d352f4663109
Pedro <pedmonte@student.42porto.com>
2023-09-13 19:04:47 +0100

    minitalk

--- MiniTalk/client.c
+++ MiniTalk/client.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
-#include <string.h>
-
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    return (0);
+    if (argc != 3)
+    {
+        fprintf(stderr, "Uso: %s <PID do servidor> <Número do Sinal (1 para SIGUSR1, 2 para SIGUSR2)>\n", argv[0]);
+        exit(1);
+    }
+
+    pid_t server_pid = atoi(argv[1]);
+    int signal_number = atoi(argv[2]);
+
+    if (signal_number == 1)
+    {
+        if (kill(server_pid, SIGUSR1) == -1)
+        {
+            perror("Erro ao enviar SIGUSR1");
+            exit(2);
+        }
+    }
+    else if (signal_number == 2)
+    {
+        if (kill(server_pid, SIGUSR2) == -1)
+        {
+            perror("Erro ao enviar SIGUSR2");
+            exit(2);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Número de sinal inválido. Use 1 para SIGUSR1 ou 2 para SIGUSR2.\n");
+        exit(1);
+    }
+
+    return 0;
 }
--- MiniTalk/testes.c
+++ MiniTalk/testes.c
@@ -1,34 +1,42 @@
-#include <signal.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 
-static void sig_usr(int); /* o mesmo handler para os 2 sinais */
+static void sig_usr(int signo);
 
 int main(void)
 {
-	if (signal(SIGUSR1, sig_usr) == SIG_ERR) {
-	fprintf(stderr, "can't catch SIGUSR1\n");
-	exit(1);
-	}
-	if (signal(SIGUSR2, sig_usr) == SIG_ERR) {
-	fprintf(stderr, "can't catch SIGUSR2\n");
-	exit(1);
-	}
-	for ( ; ; )
-		pause();
+    if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+    {
+        fprintf(stderr, "Can't catch SIGUSR1\n");
+        exit(1);
+    }
+    if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+    {
+        fprintf(stderr, "Can't catch SIGUSR2\n");
+        exit(1);
+    }
+
+    printf("Server is running with PID %d\n", getpid());
+
+    for (;;)
+        pause();
 }
 
-static void sig_usr(int signo) /* o argumento indica o sinal recebido */
+static void sig_usr(int signo)
 {
-	if (signo == SIGUSR1)
-	printf("received SIGUSR1\n");
-	else if (signo == SIGUSR2)
-	printf("received SIGUSR2\n");
-	else
-	{
-		fprintf(stderr, "received signal %d\n", signo);
-		exit(2);
-	}
-	return;
+    if (signo == SIGUSR1)
+    {
+        printf("Received SIGUSR1\n");
+    }
+    else if (signo == SIGUSR2)
+    {
+        printf("Received SIGUSR2\n");
+    }
+    else
+    {
+        fprintf(stderr, "Received signal %d\n", signo);
+        exit(2);
+    }
 }

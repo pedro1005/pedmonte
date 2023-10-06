930a7a1030975c222dc921c6b29c8f1c74d695f9
Pedro <pedmonte@student.42porto.com>
2023-10-06 10:13:50 +0100

    PS testing order

--- Push_Swap/includes/push_swap.h
+++ Push_Swap/includes/push_swap.h
@@ -31,5 +31,6 @@ void			ft_rotate_down(t_stack_node **head);
 void			ft_rra(t_stack_node **head_a);
 void			ft_rrb(t_stack_node **head_b);
 void			ft_rrr(t_stack_node **head_a, t_stack_node **head_b);
+int				ft_check_order(t_stack_node *head);
 
 #endif
--- Push_Swap/src/push_swap.c
+++ Push_Swap/src/push_swap.c
@@ -1,34 +1,54 @@
 #include "../includes/push_swap.h"
 
+int	ft_check_order(t_stack_node *head)
+{
+	int	result;
+
+	result = 1;
+	if (!head)
+		return (0);
+	while (head->next)
+	{
+		if ((head->value) > ((head->next)->value))
+			result = 0;
+		head = head->next;
+	}
+	return (result);
+}
+
+void	ft_order(t_stack_node **head_a, t_stack_node **head_b)
+{
+	t_stack_node	*temp_a;
+	//t_stack_node	*temp_b;
+
+	temp_a = *head_a;
+	//temp_b = *head_b;
+	while (temp_a->next)
+	{
+		printf("%d\n", temp_a->value);
+		ft_sa(&temp_a);
+		ft_rrb(head_b);
+		temp_a = *head_a;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	static t_stack_node	*head_a;
-	//static t_stack_node	*head_b;
+	static t_stack_node	*head_b;
+	//static t_stack_node *pivot;
 
 	head_a = NULL;
-	//head_b = NULL;
+	head_b = NULL;
+	//pivot = NULL;
 	ft_check_errors(argc); // incomplete!!
 	ft_fill_stack_a(&head_a, argv);
-	//ft_fill_stack_a(&head_b, argv);
-	ft_printf("stack a\n");
-	ft_print_stack(head_a);
-	ft_rotate_down(&head_a);
-	ft_printf("rotate down\n");
-	ft_printf("stack a\n");
-	ft_print_stack(head_a);
-	/*ft_pb(&head_a, &head_b);
-	ft_pb(&head_a, &head_b);
-	ft_printf("stack a\n");
-	ft_print_stack(head_a);
-	ft_printf("stack b\n");
-	ft_print_stack(head_b);
-	ft_rr(&head_a, &head_b);
-	ft_printf("stack a\n");
-	ft_print_stack(head_a);
-	ft_printf("stack b\n");
-	ft_print_stack(head_b);
-	*/
+	ft_order(&head_a, &head_b);
+	if (ft_check_order(head_a))
+		printf("Lista Ordenada\n");
+	else
+		printf("Lista por ordenar\n");
 	ft_free_stack(&head_a);
-	//ft_free_stack(&head_b);
+	ft_free_stack(&head_b);
 	return (0);
 }
